3fa6c29d276b035b2dd35e5b3c848ebfb18cd05f
Tim <50997599+TimBosman@users.noreply.github.com>
2025-01-30 10:03:13 +0100

    init

---
+++ sampledata.js
@@ -0,0 +1,25 @@
+POST _bulk
+{ "create" :{ "_index" : "test"}}
+{"source.ip": "0.0.0.0","port": 50,"destination.ip": "1.1.1.1"}
+{ "create" :{ "_index" : "test"}}
+{"source.ip": "0.0.0.0","port": 50,"destination.ip": "1.1.1.1"}
+{ "create" :{ "_index" : "test"}}
+{"source.ip": "0.0.0.0","port": 50,"destination.ip": "1.1.1.1"}
+{ "create" :{ "_index" : "test"}}
+{"source.ip": "0.0.0.0","port": 30,"destination.ip": "1.1.1.1"}
+{ "create" :{ "_index" : "test"}}
+{"source.ip": "0.0.0.0","port": 30,"destination.ip": "1.1.1.2"}
+{ "create" :{ "_index" : "test"}}
+{"source.ip": "0.0.0.1","port": 30,"destination.ip": "1.1.1.1"}
+{ "create" :{ "_index" : "test"}}
+{"source.ip": "0.0.0.0","port": 80,"destination.ip": "1.1.1.1"}
+{ "create" :{ "_index" : "test"}}
+{"source.ip": "0.0.0.0","port": 80,"destination.ip": "1.1.1.1"}
+{ "create" :{ "_index" : "test"}}
+{"source.ip": "0.0.0.0","port": 80,"destination.ip": "1.1.1.1"}
+{ "create" :{ "_index" : "test"}}
+{"source.ip": "0.0.0.0","port": 80,"destination.ip": "1.1.1.1"}
+{ "create" :{ "_index" : "test"}}
+{"source.ip": "0.0.0.0","port": 80,"destination.ip": "1.1.1.2"}
+{ "create" :{ "_index" : "test"}}
+{"source.ip": "0.0.0.1","port": 80,"destination.ip": "1.1.1.1"}
